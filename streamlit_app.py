11ff5865d9c921fb479242b2b291417e1bbc1458
Pradeepkommuri <149290128+Pradeepkommuri@users.noreply.github.com>
2023-11-10 00:01:07 -0500

    Update streamlit_app.py
    more cleaning

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,7 +20,10 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 
 # Display the table on the page.
 streamlit.dataframe(fruits_to_show)
-
+def get_fruity_vice_date(this_fruit_choice):
+  fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+ fruit_choice)
+  fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+  return fruityvice_normalized
 #New section to display API
 streamlit.header("Fruityvice Fruit Advice!")
 try:
@@ -30,12 +33,15 @@ try:
   else:
 #streamlit.write('The user entered ', fruit_choice)
 #import requests
-    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+ fruit_choice)
+    #fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+ fruit_choice)
 #streamlit.text(fruityvice_response.json())
 # write your own comment -what does the next line do? 
-    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+    #fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # write your own comment - what does this do?
-    streamlit.dataframe(fruityvice_normalized)
+    #streamlit.dataframe(fruityvice_normalized)
+
+    back_from_function= get_fruityvice_data(fruit_choice)
+    streamlit.dataframe(back_from_function)
 except URLError as e:
   streamlit.error()
 #donot run anything until troubleshoot completed
